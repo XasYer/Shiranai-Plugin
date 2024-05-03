1bdccbe1e2ec9f8052d6f5230548a2b96d25a7b5
小叶 <1936472877@qq.com>
2024-05-03 16:52:03 +0800

    🐞 fix: 加载失败

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 import fs from 'node:fs'
-import { Version, pluginName } from './components/index.js'
+import { Version } from './components/index.js'
 
-const files = fs.readdirSync(`./plugins/${pluginName}/apps`).filter(file => file.endsWith('.js'))
+const files = fs.readdirSync(`./plugins/${Version.pluginName}/apps`).filter(file => file.endsWith('.js'))
 
 let ret = []
 
 logger.info('-----------------')
-logger.info(`${pluginName}${Version.version}初始化~`)
+logger.info(`${Version.pluginName}${Version.version}初始化~`)
 logger.info('-------^_^-------')
 
-
 files.forEach((file) => {
-    ret.push(import(`./apps/${file}`))
+  ret.push(import(`./apps/${file}`))
 })
 
 ret = await Promise.allSettled(ret)
 
 let apps = {}
 for (let i in files) {
-    let name = files[i].replace('.js', '')
+  let name = files[i].replace('.js', '')
 
-    if (ret[i].status != 'fulfilled') {
-        logger.error(`载入插件错误：${logger.red(name)}`)
-        logger.error(ret[i].reason)
-        continue
-    }
-    apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+  if (ret[i].status != 'fulfilled') {
+    logger.error(`载入插件错误：${logger.red(name)}`)
+    logger.error(ret[i].reason)
+    continue
+  }
+  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
 }
 
 export { apps }
